cc580ee7311d1007cca2ff2d34c2b766efe23ee8
Student <student@lab-315-004.cs.csubak.edu>
2017-06-14 10:26:19 -0700

    Create a game.h file, and i removed drawStreet() and box() to abdulelahA.cpp file

--- Makefile
+++ Makefile
@@ -1,6 +1,6 @@
 all: car
 
-car: car.cpp christianR.cpp daveR.cpp alexN.cpp abdulelahA.cpp
+car: car.cpp christianR.cpp daveR.cpp alexN.cpp abdulelahA.cpp game.h
 	g++ car.cpp christianR.cpp daveR.cpp alexN.cpp abdulelahA.cpp -Wall -lX11 -lGL -lGLU -lm ./libggfonts.a -ocar
 
 clean:
--- car.cpp
+++ car.cpp
@@ -18,6 +18,7 @@
 #include <GL/glu.h>
 #include "log.h"
 #include "fonts.h"
+#include "game.h"
 
 typedef float Flt;
 typedef Flt Vec[3];
@@ -289,53 +290,6 @@ int check_keys(XEvent *e)
 	return 0;
 }
 
-void box(float w1, float h1, float d1)
-{
-	float w=w1*0.5;
-	float d=d1*0.5;
-	float h=h1*0.5;
-	//notice the normals being set
-	glBegin(GL_QUADS);
-		//top
-		glNormal3f( 0.0f, 1.0f, 0.0f);
-		glVertex3f( w, h,-d);
-		glVertex3f(-w, h,-d);
-		glVertex3f(-w, h, d);
-		glVertex3f( w, h, d);
-		// bottom
-		glNormal3f( 0.0f, -1.0f, 0.0f);
-		glVertex3f( w,-h, d);
-		glVertex3f(-w,-h, d);
-		glVertex3f(-w,-h,-d);
-		glVertex3f( w,-h,-d);
-		// front
-		glNormal3f( 0.0f, 0.0f, 1.0f);
-		glVertex3f( w, h, d);
-		glVertex3f(-w, h, d);
-		glVertex3f(-w,-h, d);
-		glVertex3f( w,-h, d);
-		// back
-		glNormal3f( 0.0f, 0.0f, -1.0f);
-		glVertex3f( w,-h,-d);
-		glVertex3f(-w,-h,-d);
-		glVertex3f(-w, h,-d);
-		glVertex3f( w, h,-d);
-		// left side
-		glNormal3f(-1.0f, 0.0f, 0.0f);
-		glVertex3f(-w, h, d);
-		glVertex3f(-w, h,-d);
-		glVertex3f(-w,-h,-d);
-		glVertex3f(-w,-h, d);
-		// Right side
-		glNormal3f( 1.0f, 0.0f, 0.0f);
-		glVertex3f( w, h,-d);
-		glVertex3f( w, h, d);
-		glVertex3f( w,-h, d);
-		glVertex3f( w,-h,-d);
-		glEnd();
-	glEnd();
-}
-
 
 void identity33(Matrix m)
 {
@@ -409,62 +363,6 @@ void trans_vector(Matrix mat, const Vec in, Vec out)
 	out[2] = f2;
 }
 
-void drawStreet()
-{
-	glPushMatrix();
-	glColor3f(0.2f, 0.2f, 0.2f);
-	float w = 5.0;
-	float d = 100.0;
-	float h = 0.0;
-	glTranslatef(0.0f, 0.0f, 0.0f);
-	glBegin(GL_QUADS);
-		//top
-		glNormal3f( 0.0f, 1.0f, 0.0f);
-		glVertex3f( w, h,-d);
-		glVertex3f(-w, h,-d);
-		glVertex3f(-w, h, d);
-		glVertex3f( w, h, d);
-	glEnd();
-	//double yellow line
-	glColor3f(0.8f, 0.8f, 0.2f);
-	w = 0.1;
-	d = 100.0;
-	h = 0.01;
-	glPushMatrix();
-	glTranslatef(-0.15f, 0.0f, 0.0f);
-	glBegin(GL_QUADS);
-		//top
-		glNormal3f( 0.0f, 1.0f, 0.0f);
-		glVertex3f( w, h,-d);
-		glVertex3f(-w, h,-d);
-		glVertex3f(-w, h, d);
-		glVertex3f( w, h, d);
-	glEnd();
-	glPopMatrix();
-	glPushMatrix();
-	glTranslatef(0.15f, 0.0f, 0.0f);
-	glBegin(GL_QUADS);
-		//top
-		glNormal3f( 0.0f, 1.0f, 0.0f);
-		glVertex3f( w, h,-d);
-		glVertex3f(-w, h,-d);
-		glVertex3f(-w, h, d);
-		glVertex3f( w, h, d);
-	glEnd();
-	glPopMatrix();
-	//guard rails
-	glColor3f(1.0f, 1.0f, 1.0f);
-	for (int i=0; i<40; i++) {
-		glPushMatrix();
-		glTranslatef(6.0f, -0.5f, (float)-i*2.5);
-		box(0.2, 5.0, 0.2);
-		glPopMatrix();
-		glPushMatrix();
-		glTranslatef(-6.0f, -0.5f, (float)-i*2.5);
-		box(0.2, 5.0, 0.2);
-		glPopMatrix();
-	}
-}
 
 void physics(void)
 {
