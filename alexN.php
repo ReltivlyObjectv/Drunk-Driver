cafd7a07f4617d973435cd600d697cf44b28de90
Ethereal15 <xionnguyen@gmail.com>
2017-06-17 14:29:41 -0700

    Added some php stuff

--- alexN.php
+++ alexN.php
@@ -1,3 +1,32 @@
-<?php
-echo "Testing"
- ?>
+
+<!DOCTYPE HTML>  
+<html>
+<head>
+<style>
+.error {color: #FF0000;}
+</style>
+</head>
+<body>  
+
+
+<h2>Drunk Driving</h2>
+
+Current time: 02:28:20pm<p><span class="error">* required field.</span></p>
+<form method="post" action="/~anguyen/3350/test.php">  
+  Name: <input type="text" name="name">
+  <span class="error">* Name is required</span>
+  <br><br>
+  Username: <input type="text" name="uid">
+  <span class="error">* Username is required</span>
+  <br><br>
+  Gender:
+  <input type="radio" name="gender" value="female">Female
+  <input type="radio" name="gender" value="male">Male
+  <span class="error">* </span>
+  <br><br>
+  <input type="submit" name="submit" value="Submit">  
+</form>
+
+<h2>Input:</h2><br><br><br>male
+</body>
+</html>
