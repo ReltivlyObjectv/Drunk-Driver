46e4ec2f0ae47b42321153d8fdde56ccf61a9297
Ethereal15 <xionnguyen@gmail.com>
2017-06-17 17:04:38 -0700

    Update alexN.cpp

--- alexN.cpp
+++ alexN.cpp
@@ -1,10 +1,10 @@
-Author: Alexander Nguyen
+/*Author: Alexander Nguyen
 Modify by: Alexander Nguyen
 Date:6/17/2017
 
 Purpose: Create a start menu that will necessary function
 also have it connect to php file and upload score.
-
+*/
 #include <iostream>
 #include <stdio.h>
 #include <string.h>
