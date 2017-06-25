61880b5d6d2a70388f6a926b714cded9891add42
Alex <anguyen@cs.csubak.edu>
2017-06-24 17:48:56 -0700

    updated changes

--- alexN.cpp
+++ alexN.cpp
@@ -1,8 +1,8 @@
 /*Author: Alexander Nguyen
   Modify by: Alexander Nguyen
-  Date:6/17/2017
+Date:6/17/2017
 
-  Purpose: Create a start menu that will have Start, High Score, Credits and Exit
+Purpose: Create a start menu that will have Start, High Score, Credits and Exit
  */
 
 /* 6/17/17 : Borrow some definition and function from bship and moditify it
@@ -23,27 +23,25 @@
 
 
 int game = 1;
-int startgame = 1;
 int done = 0;
 //4 buttons Start, High Score, Credits, Exit
 #define MAXBUTTONS 4
 
 //button click from bship framework
 typedef struct t_button {
-    Rect r;
-    char text[32];
-    int over;
-    int down;
-    int mouse[2];
-    float color[3];
-    float dcolor[3];
-    int click;
-    unsigned int text_color;
+	Rect r;
+	char text[32];
+	int over;
+	int down;
+	int mouse[2];
+	float color[3];
+	float dcolor[3];
+	int click;
+	unsigned int text_color;
 }Button;
 int nbuttons=0;
 
 Button button[MAXBUTTONS];
-void gamemenu(void);
 void mouse_click(int action);
 void check_button(XEvent *e);
 
@@ -59,33 +57,33 @@ bool credits = false;
 //Start Menu
 void gamemenu(void)
 {
-    glPushMatrix();
-    glBindTexture(GL_TEXTURE_2D,menuTexture);
-    glBegin(GL_QUADS); 
-    glTexCoord2f(0.0f,0.0f); glVertex2i(0,0); 
-    glTexCoord2f(0.0f,5.0f); glVertex2i(0,g.yres);
-    glTexCoord2f(50.0f,5.0f); glVertex2i(g.xres,g.yres);
-    glTexCoord2f(5.0f,0.0f); glVertex2i(g.xres,0);
-    glEnd();
-    glPopMatrix();
-    if(game) {	
-    	button_init();
-    }
-    button_render();
-  
+	glPushMatrix();
+	glBindTexture(GL_TEXTURE_2D,menuTexture);
+	glBegin(GL_QUADS); 
+	glTexCoord2f(0.0f,0.0f); glVertex2i(0,0); 
+	glTexCoord2f(0.0f,5.0f); glVertex2i(0,g.yres);
+	glTexCoord2f(50.0f,5.0f); glVertex2i(g.xres,g.yres);
+	glTexCoord2f(5.0f,0.0f); glVertex2i(g.xres,0);
+	glEnd();
+	glPopMatrix();
+	if(game) {	
+		button_init();
+	}
+	button_render();
+
 }
 
 //Button width
-#define BUTTON_W 100;
+#define BUTTON_W 100
 //Button height
-#define BUTTON_H 50;
+#define BUTTON_H 50
 //Button bottom
-#define BUTTON_B 500;
+#define BUTTON_B 500
 
 //Button will be drawn onto the menu 
 void button_init(void)
 {
-    //add button function to initiate
+	//add button function to initiate
 	//Start button
 	button[nbuttons].r.width = BUTTON_W;
 	button[nbuttons].r.height = BUTTON_H;
@@ -187,33 +185,33 @@ void button_init(void)
 //Render the buttons
 void button_render(void)
 {
-    //add render function to render button
-    Rect r;
-    for (int i = 0; i < nbuttons; i++) {
-	    if (button[i].over) {
-		    //create the highlight color of button
-		    glColor3f(5.0f, 0.0f, 23.0f);
-		    //draw a highlight around the button
-		    glLineWidth(2);
-		    glBegin(GL_LINE_LOOP);
-		    glVertex2i(button[i].r.left-2,  button[i].r.bot-2);
-		    glVertex2i(button[i].r.left-2,  button[i].r.top+2);
-		    glVertex2i(button[i].r.right+2, button[i].r.top+2);
-		    glVertex2i(button[i].r.right+2, button[i].r.bot-2);
-		    glVertex2i(button[i].r.left-2,  button[i].r.bot-2);
-		    glEnd();
-		    glLineWidth(1);
+	//add render function to render button
+	Rect r;
+	for (int i = 0; i < nbuttons; i++) {
+		if (button[i].over) {
+			//create the highlight color of button
+			glColor3f(5.0f, 0.0f, 23.0f);
+			//draw a highlight around the button
+			glLineWidth(2);
+			glBegin(GL_LINE_LOOP);
+			glVertex2i(button[i].r.left-2,  button[i].r.bot-2);
+			glVertex2i(button[i].r.left-2,  button[i].r.top+2);
+			glVertex2i(button[i].r.right+2, button[i].r.top+2);
+			glVertex2i(button[i].r.right+2, button[i].r.bot-2);
+			glVertex2i(button[i].r.left-2,  button[i].r.bot-2);
+			glEnd();
+			glLineWidth(1);
 		}
-	    if (button[i].down) {
-		    glColor3fv(button[i].dcolor);
-	    } else {
-		    glColor3fv(button[i].color);
-	    }
-	    glBegin(GL_QUADS);
-			glVertex2i(button[i].r.left,  button[i].r.bot);
-			glVertex2i(button[i].r.left,  button[i].r.top);
-			glVertex2i(button[i].r.right, button[i].r.top);
-			glVertex2i(button[i].r.right, button[i].r.bot);
+		if (button[i].down) {
+			glColor3fv(button[i].dcolor);
+		} else {
+			glColor3fv(button[i].color);
+		}
+		glBegin(GL_QUADS);
+		glVertex2i(button[i].r.left,  button[i].r.bot);
+		glVertex2i(button[i].r.left,  button[i].r.top);
+		glVertex2i(button[i].r.right, button[i].r.top);
+		glVertex2i(button[i].r.right, button[i].r.bot);
 		glEnd();
 		r.left = button[i].r.centerx;
 		r.bot  = button[i].r.centery-8;
@@ -224,7 +222,7 @@ void button_render(void)
 			ggprint16(&r, 0, button[i].text_color, button[i].text);
 		}
 	}
-	
+
 }
 
 
@@ -251,7 +249,7 @@ void check_button(XEvent *e)
 			lbutton = 1;
 		}
 		//when mouse right button is pressed
-		if (e-xbutton.button == 3) {
+		if (e->xbutton.button == 3) {
 			rbutton = 1;
 		}
 	}
@@ -263,15 +261,15 @@ void check_button(XEvent *e)
 	}
 	else if (x == savex && y == savey)
 		return;
-	
+
 	for (i=0; i<nbuttons; i++) {
 		button[i].over=0;
 		button[i].down=0;
 
 		if (x >= button[i].r.left &&
-			x <= button[i].r.right &&
-			y >= button[i].r.bot &&
-			y <= button[i].r.top) {
+				x <= button[i].r.right &&
+				y >= button[i].r.bot &&
+				y <= button[i].r.top) {
 			button[i].over=1;
 			break;
 		}
@@ -286,33 +284,35 @@ void check_button(XEvent *e)
 //Will excuted when one of the state holds true
 void mouse_click(int action)
 {
- if(startgame)
- {
-    if (action == 1) {
-      //center of menu
-      for (int i=0; i<nbuttons; i++) {
-	if (button[i].over) {
-        	button[i].down = 1;
-        	button[i].click = 1;
-        	if (i == 0) {
-          		//start the game
-          		startgame = 0;
-        	} 
-        	if (i == 1) {
-	  		//show high score
-          		high_score = true;
-        	}
-        	if (i == 2) {
-	  		//show credits
-          		credits = true;
-        	}
-        	if (i == 3) {
-          		//Exit the game
-          		done = 1;
-        	}
-     	    }
-    	}
-      }
-   }
+
+int startgame = 1;
+	if(startgame)
+	{
+		if (action == 1) {
+			//center of menu
+			for (int i=0; i<nbuttons; i++) {
+				if (button[i].over) {
+					button[i].down = 1;
+					button[i].click = 1;
+					if (i == 0) {
+						//start the game
+						startgame = 0;
+					} 
+					if (i == 1) {
+						//show high score
+						high_score = true;
+					}
+					if (i == 2) {
+						//show credits
+						credits = true;
+					}
+					if (i == 3) {
+						//Exit the game
+						done = 1;
+					}
+				}
+			}
+		}
+	}
 }
-      
+
