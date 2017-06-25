51e7c77ed5766604038e5dbc1570c1b72e37e97b
Ethereal15 <xionnguyen@gmail.com>
2017-06-24 17:31:26 -0700

    Updated cpp changes, added render codes

--- alexN.cpp
+++ alexN.cpp
@@ -6,11 +6,12 @@
  */
 
 /* 6/17/17 : Borrow some definition and function from bship and moditify it
- * 6/21/17 : add ppm image of menu, start button init function move check mouse
- * 6/22/17 :Move check mouse back and add the function to it to extend check mouse
- *
- *
- *
+ * 6/21/17 : Add ppm image of menu, start button init function move check mouse
+ * 6/22/17 : Move check mouse back and add the function to it to extend check mouse
+ * 6/24/17 : Created and update check_button and initalize function in check_mouse
+ *	     Finish button_init and button_render, will be adding addition 
+ *           Game over menu/screen function, will be moving #include and definition 
+ *	     then clean up codes to K&R.
  */
 
 //Will fix the include on Saturday
@@ -24,7 +25,8 @@
 int game = 1;
 int startgame = 1;
 int done = 0;
-#define MAXBUTTONS 8
+//4 buttons Start, High Score, Credits, Exit
+#define MAXBUTTONS 4
 
 //button click from bship framework
 typedef struct t_button {
@@ -57,23 +59,14 @@ bool credits = false;
 //Start Menu
 void gamemenu(void)
 {
-
     glPushMatrix();
-    glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
-    glClearDepth(1.0f);
-    glClear(GL_COLOR_BUFFER_BIT);
-    glEnable(GL_COLOR_MATERIAL);
-    glShadeModel(GL_SMOOTH);
-    glDisable(GL_LIGHTING);
     glBindTexture(GL_TEXTURE_2D,menuTexture);
     glBegin(GL_QUADS); 
     glTexCoord2f(0.0f,0.0f); glVertex2i(0,0); 
     glTexCoord2f(0.0f,5.0f); glVertex2i(0,g.yres);
     glTexCoord2f(50.0f,5.0f); glVertex2i(g.xres,g.yres);
     glTexCoord2f(5.0f,0.0f); glVertex2i(g.xres,0);
-  
     glEnd();
-    glBindTexture(GL_TEXTURE_2D, 0);
     glPopMatrix();
     if(game) {	
     	button_init();
@@ -82,15 +75,22 @@ void gamemenu(void)
   
 }
 
-//Will modify once menu is up and running
+//Button width
+#define BUTTON_W 100;
+//Button height
+#define BUTTON_H 50;
+//Button bottom
+#define BUTTON_B 500;
+
+//Button will be drawn onto the menu 
 void button_init(void)
 {
     //add button function to initiate
 	//Start button
-	button[nbuttons].r.width = 100;
-	button[nbuttons].r.height = 50;
+	button[nbuttons].r.width = BUTTON_W;
+	button[nbuttons].r.height = BUTTON_H;
 	button[nbuttons].r.left = g.xres/2 - button[nbuttons].r.width/2;
-	button[nbuttons].r.bot = 500;
+	button[nbuttons].r.bot = BUTTON_B;
 	button[nbuttons].r.right = 
 		button[nbuttons].r.left + button[nbuttons].r.width;
 	button[nbuttons].r.top = 
@@ -111,10 +111,10 @@ void button_init(void)
 	button[nbuttons].text_color = 0x0000ffff;
 	nbuttons++;
 	//High Score button
-	button[nbuttons].r.width = 300;
-	button[nbuttons].r.height = 50;
+	button[nbuttons].r.width = (BUTTON_W + 200);
+	button[nbuttons].r.height = BUTTON_H;
 	button[nbuttons].r.left = g.xres/2 - button[nbuttons].r.width/2;
-	button[nbuttons].r.bot = 400;
+	button[nbuttons].r.bot = (BUTTON_B - 100);
 	button[nbuttons].r.right = 
 		button[nbuttons].r.left + button[nbuttons].r.width;
 	button[nbuttons].r.top = 
@@ -135,10 +135,10 @@ void button_init(void)
 	button[nbuttons].text_color = 0x0000ffff;
 	nbuttons++;
 	//Credits button
-	button[nbuttons].r.width = 150;
-	button[nbuttons].r.height = 50;
+	button[nbuttons].r.width = (BUTTON_W + 50);
+	button[nbuttons].r.height = BUTTON_H;
 	button[nbuttons].r.left = g.xres/2 - button[nbuttons].r.width/2;
-	button[nbuttons].r.bot = 300;
+	button[nbuttons].r.bot = (BUTTON_B - 200);
 	button[nbuttons].r.right = 
 		button[nbuttons].r.left + button[nbuttons].r.width;
 	button[nbuttons].r.top = 
@@ -159,10 +159,10 @@ void button_init(void)
 	button[nbuttons].text_color = 0x0000ffff;
 	nbuttons++;
 	//Quit button
-	button[nbuttons].r.width = 100;
-	button[nbuttons].r.height = 50;
+	button[nbuttons].r.width = BUTTON_W;
+	button[nbuttons].r.height = BUTTON_H;
 	button[nbuttons].r.left = g.xres/2 - button[nbuttons].r.width/2;
-	button[nbuttons].r.bot = 200;
+	button[nbuttons].r.bot = (BUTTON_B - 300);
 	button[nbuttons].r.right = 
 		button[nbuttons].r.left + button[nbuttons].r.width;
 	button[nbuttons].r.top = 
@@ -184,13 +184,51 @@ void button_init(void)
 	nbuttons++;
 }
 
+//Render the buttons
 void button_render(void)
 {
     //add render function to render button
+    Rect r;
+    for (int i = 0; i < nbuttons; i++) {
+	    if (button[i].over) {
+		    //create the highlight color of button
+		    glColor3f(5.0f, 0.0f, 23.0f);
+		    //draw a highlight around the button
+		    glLineWidth(2);
+		    glBegin(GL_LINE_LOOP);
+		    glVertex2i(button[i].r.left-2,  button[i].r.bot-2);
+		    glVertex2i(button[i].r.left-2,  button[i].r.top+2);
+		    glVertex2i(button[i].r.right+2, button[i].r.top+2);
+		    glVertex2i(button[i].r.right+2, button[i].r.bot-2);
+		    glVertex2i(button[i].r.left-2,  button[i].r.bot-2);
+		    glEnd();
+		    glLineWidth(1);
+		}
+	    if (button[i].down) {
+		    glColor3fv(button[i].dcolor);
+	    } else {
+		    glColor3fv(button[i].color);
+	    }
+	    glBegin(GL_QUADS);
+			glVertex2i(button[i].r.left,  button[i].r.bot);
+			glVertex2i(button[i].r.left,  button[i].r.top);
+			glVertex2i(button[i].r.right, button[i].r.top);
+			glVertex2i(button[i].r.right, button[i].r.bot);
+		glEnd();
+		r.left = button[i].r.centerx;
+		r.bot  = button[i].r.centery-8;
+		r.center = 1;
+		if (button[i].down) {
+			ggprint16(&r, 0, button[i].text_color, "Pressed!");
+		} else {
+			ggprint16(&r, 0, button[i].text_color, button[i].text);
+		}
+	}
+	
 }
 
 
-//move check mouse from car.cpp to this .cpp
+//Created a check button to check to see if mouse will click
 void check_button(XEvent *e)
 {
 	static int savex = 0;
@@ -200,16 +238,35 @@ void check_button(XEvent *e)
 	int i;
 	int lbutton=0;
 	int rbutton=0;
-
+	//y is backward
+	y = g.yres - y;
+	//when mouse button is release
+	if (e->type == ButtonRelease) {
+		mouse_click(2);
+		return;
+	}
+	if (e->type == ButtonPress) {
+		//when mouse left button is pressed
+		if (e->xbutton.button == 1) {
+			lbutton = 1;
+		}
+		//when mouse right button is pressed
+		if (e-xbutton.button == 3) {
+			rbutton = 1;
+		}
+	}
+	//check to see if mouse move either in x or y direction
 	if (savex != e->xbutton.x || savey != e->xbutton.y) {
-	savex=e->xbutton.x;
-	savey=e->xbutton.y;
+		savex=e->xbutton.x;
+		savey=e->xbutton.y;
 
 	}
+	else if (x == savex && y == savey)
+		return;
+	
 	for (i=0; i<nbuttons; i++) {
 		button[i].over=0;
 		button[i].down=0;
-
 
 		if (x >= button[i].r.left &&
 			x <= button[i].r.right &&
@@ -225,32 +282,37 @@ void check_button(XEvent *e)
 		mouse_click(2);
 
 }
+
+//Will excuted when one of the state holds true
 void mouse_click(int action)
 {
- 
+ if(startgame)
+ {
     if (action == 1) {
       //center of menu
       for (int i=0; i<nbuttons; i++) {
-        button[i].down = 1;
-        button[i].click = 1;
-        if (i == 0) {
-          //start the game
-          startgame = 0;
-        } 
-        if (i == 1) {
-	  //show high score
-          high_score = true;
-        }
-        if (i == 2) {
-	  //show credits
-          credits = true;
-        }
-        if (i == 3) {
-          //Exit the game
-          done = 1;
-        }
+	if (button[i].over) {
+        	button[i].down = 1;
+        	button[i].click = 1;
+        	if (i == 0) {
+          		//start the game
+          		startgame = 0;
+        	} 
+        	if (i == 1) {
+	  		//show high score
+          		high_score = true;
+        	}
+        	if (i == 2) {
+	  		//show credits
+          		credits = true;
+        	}
+        	if (i == 3) {
+          		//Exit the game
+          		done = 1;
+        	}
+     	    }
+    	}
       }
-    }  
+   }
 }
-
       
