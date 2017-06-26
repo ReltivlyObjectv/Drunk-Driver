9a8515169f6611d4fd312487d96b7d5a426ca751
ReltivlyObjectv <me@relativelyobjective.tech>
2017-06-26 14:13:02 -0700

    Added BAC mechanics
    also commented out unneeded printfs

--- christianR.cpp
+++ christianR.cpp
@@ -40,10 +40,13 @@
 #define MIN_MOVEMENT 0.1
 #define ROAD_WIDTH 4
 #define SPEED_TO_MPH_MULT 100
-#define FPS 30
+#define FPS 35
 #define OBSTACLE_DEPTH 5
 #define OBSTACLE_WIDTH 5
 #define CAMERA_HEIGHT 1
+#define COOLDOWN_DRINK 100
+#define COOLDOWN_BAC .000008
+#define BAC_PER_BEER .025
 
 bool ControlManager::movingLeft,
      ControlManager::movingRight,
@@ -107,38 +110,75 @@ double ControlManager::calculateSwerveModifier(Game& g)
 	if(hittingObject){
 		//Cannot use tires in the air
 		return 0.0;
+	}else if (g.getInebriationLevel() == 0) {
+		return 0.0;
 	}
-	double inebriationLevelModifier = (1 + (g.inebriationLevel / 3));
-	return 0.05 * sin(g.cameraPosition[2] * 0.25)  * inebriationLevelModifier;
+	double inebriationLevelModifier = (1 + (g.getInebriationLevel() / 3));
+	return .001 * 5 * sin(g.cameraPosition[2] * 0.25)  * inebriationLevelModifier;
 }
 void ControlManager::applyControls(Game& g, int key, bool isPress)
 {
 	switch (key) {
 		case XK_Right:
 		case XK_d:
-			printf("Key is pressed: %s (%d)\n", "Right", key);
+			if (isPress) {
+//				printf("Key is pressed: %s (%d)\n", "Right", key);
+			} else {
+//				printf("Key released: %s (%d)\n", "Right", key);
+			}
 			ControlManager::movingRight = isPress;
 			break;
 		case XK_Left:
 		case XK_a:
-			printf("Key is pressed: %s (%d)\n", "Left", key);
+			if (isPress) {
+//				printf("Key is pressed: %s (%d)\n", "Left", key);
+			} else {
+//				printf("Key released: %s (%d)\n", "Left", key);
+			}
 			ControlManager::movingLeft = isPress;
 			break;
 		case XK_Up:
 		case XK_w:
-			printf("Key is pressed: %s (%d)\n", "Speed Up", key);
+			if (isPress) {
+//				printf("Key is pressed: %s (%d)\n", "Speed Up", key);
+//				printf("Speeding Up. Velocity: %.5f\n", g.speed);
+			} else {
+//				printf("Key released: %s (%d)\n", "Speed Up", key);
+			}
 			ControlManager::speedingUp = isPress;
-			printf("Speeding Up. Velocity: %.5f\n", g.speed);
 			break;
 		case XK_Down:
 		case XK_s:
-			printf("Key is pressed: %s (%d)\n", "Slow Down", key);
+			if (isPress) {
+//				printf("Key is pressed: %s (%d)\n", "Slow Down", key);
+//				printf("Slowing Down. Velocity: %.5f\n", g.speed);
+			} else {
+			
+//				printf("Key is released: %s (%d)\n", "Slow Down", key);
+			}
 			ControlManager::slowingDown = isPress;
-			printf("Slowing Down. Velocity: %.5f\n", g.speed);
 			break;
 		case XK_h:
-			printf("Key is pressed: %s (%d)\n", "Hit Animation", key);
-			ControlManager::playAnimationHit();
+			if (isPress) {
+//				printf("Key is pressed: %s (%d)\n", "Hit Animation", key);
+				ControlManager::playAnimationHit();
+			} else {
+				printf("Key released: %s (%d)\n", "Hit Animation", key);
+			}
+			break;
+		case XK_j:
+			if (isPress) {
+				if (g.cooldownDrink == 0) {
+					printf("Key is pressed: %s (%d)\n", "Drinking a nice, cold bev", key);
+					g.bloodAlcoholContent += BAC_PER_BEER;
+					g.minimumBAC += BAC_PER_BEER * .33;
+					g.cooldownDrink = COOLDOWN_DRINK;
+				} else {
+//					printf("Key is pressed: %s (%d)\n", "Cannot drink (cooldown)", key);
+				}
+			} else {
+//				printf("Key is released: %s (%d)\n", "Drinking button", key);
+			}
 			break;
 		case XK_Escape:
 			printf("Key is pressed: %s (%d)\n", "Escape", key);
@@ -191,8 +231,35 @@ double Game::getMPH()
 {
 	return speed * SPEED_TO_MPH_MULT;
 }
-double Game::getDistanceMiles(){
+double Game::getDistanceMiles()
+{
 	return distanceTraveled / SPEED_TO_MPH_MULT / FPS;
+}
+int Game::getInebriationLevel()
+{
+	if(bloodAlcoholContent < 0.08){
+		return 0;
+	} else if (bloodAlcoholContent < .1) {
+		return 1;
+	} else if (bloodAlcoholContent < .14) {
+		return 2;
+	} else if (bloodAlcoholContent < .2) {
+		return 3;
+	} else if (bloodAlcoholContent < .3) {
+		return 4;
+	} else {
+		return 5;
+	}
+}
+void Game::updateCooldowns()
+{
+	//Called every frame -- reduces each cooldown by one
+	if (cooldownDrink > 0) {
+		cooldownDrink--;
+	}
+	if (bloodAlcoholContent > minimumBAC) {
+		bloodAlcoholContent -= COOLDOWN_BAC;
+	}
 }
 RoadObstacle::RoadObstacle(double roadPosLR, double roadPosDistance) 
 {
@@ -239,10 +306,23 @@ void drawDebugInfo(Game& g)
 	//Vertical position
 	sprintf(buffer, "Road Position (Vertical): %.3f", g.cameraPosition[1]);
 	ggprint8b(&debugStats, 16, 0x00FFFF00, buffer);
+	//Inebriation Level
+	sprintf(buffer, "Drunkness Level: %d", g.getInebriationLevel());
+	ggprint8b(&debugStats, 16, 0x00FFFF00, buffer);
+	//BAC
+	sprintf(buffer, "BAC: %.3f", g.bloodAlcoholContent);
+	ggprint8b(&debugStats, 16, 0x00FFFF00, buffer);
+	//BAC
+	sprintf(buffer, "Minimum BAC: %.3f", g.minimumBAC);
+	ggprint8b(&debugStats, 16, 0x00FFFF00, buffer);
+	//Drinking Cooldown
+	sprintf(buffer, "Cooldown (Drinking): %d", g.cooldownDrink);
+	ggprint8b(&debugStats, 16, 0x00FFA500, buffer);
 	//Controls
 	ggprint8b(&debugStats, 16, 0x0000FF00, "W/Up - Speed Up");
 	ggprint8b(&debugStats, 16, 0x0000FF00, "S/Down - Slow Down");
 	ggprint8b(&debugStats, 16, 0x0000FF00, "A/Left - Speed Up");
 	ggprint8b(&debugStats, 16, 0x0000FF00, "D/Right - Turn Right");
 	ggprint8b(&debugStats, 16, 0x0000FF00, "H - Hit Animation Test");
+	ggprint8b(&debugStats, 16, 0x0000FF00, "J - Drink a beer");
 }
