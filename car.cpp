9f8b37f2e010168813a2106866f911c3d5f18de8
ReltivlyObjectv <me@relativelyobjective.tech>
2017-07-03 13:18:08 -0700

    Added call to game_over on gameState == GAMEOVER

--- car.cpp
+++ car.cpp
@@ -382,7 +382,7 @@ void render(void)
 		}
 		glPopAttrib();
 		glEnable(GL_DEPTH_TEST);
-	} else if (g.gameState == UNPAUSED || g.gameState == PAUSED) {
+	} else if (g.gameState == UNPAUSED || g.gameState == PAUSED || g.gameState == GAMEOVER) {
 		//printf("Rendering Game...\n");
 		Rect r;
 		glClear(GL_DEPTH_BUFFER_BIT | GL_COLOR_BUFFER_BIT);
@@ -424,6 +424,8 @@ void render(void)
 		blackoutScreen(g);
 		if (g.gameState == PAUSED) {
 			drawPauseMenu(g);
+		} else if (g.gameState == GAMEOVER) {
+			game_over();
 		}
 	}
 }
