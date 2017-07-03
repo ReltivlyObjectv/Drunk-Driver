d5973df797d7118cdbf609b264c3f22de4aab65f
ReltivlyObjectv <me@relativelyobjective.tech>
2017-07-02 23:57:20 -0700

    Added GLM and OBJ loading for roadside obstacles

--- christianR.cpp
+++ christianR.cpp
@@ -27,6 +27,9 @@
 #include <string>
 #include <sstream>
 #include <unistd.h>
+#include <vector>
+
+#include "glm/glm.hpp"
 #include "game.h"
 #include "fonts.h"
 #include "ppm.h"
@@ -493,19 +496,86 @@ GLuint Object3d::loadBMP(std::string path)
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
 	return textureID;
 }
+void Object3d::loadOBJ(std::string path)
+{
+	//Done with the assistance of http://www.opengl-tutorial.org/beginners-tutorials/tutorial-7-model-loading/
+	printf("Loading model: %s\n", path.c_str());
+	std::vector<unsigned int> vertexIndices, uvIndices, normalIndices;
+	std::vector<glm::vec3> temp_vertices;
+	std::vector<glm::vec2> temp_uvs;
+	std::vector<glm::vec3> temp_normals;
+	FILE* file = fopen(path.c_str(), "r");
+	if (file == NULL) {
+		printf("Model could not be opened: %s\n", path.c_str());
+		working = false;
+		return;
+	}
+	while(true){
+		char lineHeader[256];
+		// read the first word of the line
+		int res = fscanf(file, "%s", lineHeader);
+		if (res == EOF) {
+			break; // EOF = End Of File. Quit the loop.
+		} else {
+			if (strcmp(lineHeader, "v") == 0) {
+				glm::vec3 vertex;
+				fscanf(file, "%f %f %f\n", &vertex.x, &vertex.y, &vertex.z );
+				temp_vertices.push_back(vertex);
+			} else if (strcmp( lineHeader, "vt") == 0) {
+				glm::vec2 uv;
+				fscanf(file, "%f %f\n", &uv.x, &uv.y);
+				temp_uvs.push_back(uv);
+			} else if (strcmp(lineHeader, "vn") == 0) {
+				glm::vec3 normal;
+				fscanf(file, "%f %f %f\n", &normal.x, &normal.y, &normal.z );
+				temp_normals.push_back(normal);
+			} else if (strcmp(lineHeader, "f") == 0){
+				std::string vertex1, vertex2, vertex3;
+				unsigned int vertexIndex[3], uvIndex[3], normalIndex[3];
+				int matches = fscanf(file, "%d/%d/%d %d/%d/%d %d/%d/%d\n", 
+						&vertexIndex[0], &uvIndex[0], &normalIndex[0], 
+						&vertexIndex[1], &uvIndex[1], &normalIndex[1], 
+						&vertexIndex[2], &uvIndex[2], &normalIndex[2]);
+				if (matches != 9){
+					printf("File can't be read by simple parser: %s\n", path.c_str());
+					working = false;
+					return;
+				}
+				vertexIndices.push_back(vertexIndex[0]);
+				vertexIndices.push_back(vertexIndex[1]);
+				vertexIndices.push_back(vertexIndex[2]);
+				uvIndices.push_back(uvIndex[0]);
+				uvIndices.push_back(uvIndex[1]);
+				uvIndices.push_back(uvIndex[2]);
+				normalIndices.push_back(normalIndex[0]);
+				normalIndices.push_back(normalIndex[1]);
+				normalIndices.push_back(normalIndex[2]);
+			}
+		}
+	}
+	for (unsigned int i=0; i<vertexIndices.size(); i++) {
+		unsigned int vertexIndex = vertexIndices[i];
+		glm::vec3 vertex = temp_vertices[vertexIndex-1];
+		vertices.push_back(vertex);
+	}
+	for (unsigned int i=0; i<uvIndices.size(); i++) {
+		unsigned int uvIndex = uvIndices[i];
+		glm::vec2 uv = temp_uvs[uvIndex-1];
+		uvs.push_back(uv);
+	}
+	for (unsigned int i=0; i<normalIndices.size(); i++) {
+		unsigned int normalIndex = normalIndices[i];
+		glm::vec3 normal = temp_normals[normalIndex-1];
+		normals.push_back(normal);
+	}
+}
 Object3d::Object3d(std::string path, std::string texPath, double roadPosLR, double roadPosDistance)
 {
 	roadPositionLR = roadPosLR;
 	roadPositionDistance = roadPosDistance;
-	FILE* file = fopen(path.c_str(), "r");
-	if (file == NULL) {
-		working = false;
-		printf("File could not be found!\n");
-		return;
-	}
-	GLuint texture = loadBMP(texPath);
-	printf("printing to prevent warnings until function finished: %u\n", texture);
-	//TODO load model
+	texture = loadBMP(texPath);
+	loadOBJ(path);
+	//glBufferData(GL_ARRAY_BUFFER, vertices.size() * sizeof(glm::vec3), &vertices[0], GL_STATIC_DRAW);
 }
 bool Object3d::isWorking()
 {
